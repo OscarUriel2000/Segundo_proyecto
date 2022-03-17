4a8f4e13040e7edd11b49d808a41a11339ea65c2
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2022-03-17 16:10:06 -0600

    Operador spread

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  const miObjeto = {
+  const persona = {
     nombre: "Bartholome",
     apellido: "Simpson",
     edad: 10,
@@ -10,10 +10,29 @@ const App = () => {
       ciudad: "Springfield",
     },
   };
-  const { edad, nombre, apellido } = miObjeto;
-  const { calle, numExt, ciudad } = miObjeto.direccion;
-  const familia = ["Homer", "Margorie", "Lisa", "Maggie"] ;
-  const [padre, madre, hermana1, hermana2] = familia;
+  const contactoPersona = {
+    telefono: "2871234567",
+    email: "bart.s@live.com",
+    web: "www.thesimpsons.com",
+  };
+  
+  const infoPersona = { ...persona, ...contactoPersona, empresa: "Disney" };
+  
+  infoPersona.apodo = "Bart";
+
+  infoPersona.telefono= "5555555555"
+
+  //console.table(infoPersona);
+
+  const { edad, nombre, apellido } = persona;
+  const { calle, numExt, ciudad } = persona.direccion;
+  const humanos = ["Homer", "Margorie", "Lisa", "Maggie"] ;
+  const [padre, madre, hermana1, hermana2] = humanos;
+  const mascotas = ["Santa's help", "Snow ball"];
+
+  const familia = [...humanos, ...mascotas];
+  console.log(familia);
+  
   return (
     <div className="App">
     <h3> {`Mi nombre es ${nombre} ${apellido}, tengo ${edad} años y vivo en ${calle} numero ${numExt}, ${ciudad}`}</h3>
@@ -22,7 +41,7 @@ const App = () => {
     <ul>
       <li>`Mi padre: ${padre}`</li>
       <li>`Mi madre: ${madre}`</li>
-      <li>`Mis hermanas: ${hermana1} y ${hermana2}`</li>
+      <li>`Mis hermanas: ${hermana1} y ${hermana2}'</li>
     </ul>
     </div>
   );
